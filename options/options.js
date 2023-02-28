0bcaae41261c8d8ad04f100d8faf02a4598ce843
Crowdbotics <engineering@crowdbotics.com>
2023-02-28 20:10:28 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -2,9 +2,11 @@ export const globalOptions = {
   name: "sprint35a-dev-65690",
   url: "https://sprint35a-dev-65690.botics.co",
   api: "https://sprint35a-dev-65690.botics.co/api/v1"
-};
+}
+
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
-  }
-};
+  },
+  undefined: { title: "App", copy: "Routes available!", styles: styles }
+}
